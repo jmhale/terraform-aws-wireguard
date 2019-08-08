de85b65bec1668b49d89b2928fd7ce73075da6e0
atrull <git.alex@trull.org>
2019-08-08 16:25:42 +0100

    Added to the outputs and separated the two security group ids into more meaningful ones.

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-output "vpn_sg_id" {
+output "vpn_sg_admin_id" {
   value = "${aws_security_group.sg_wireguard_admin.id}"
   description = "ID of the internal Security Group to associate with other resources needing to be accessed on VPN."
+}
+
+output "vpn_sg_external_id" {
+  value = "${aws_security_group.sg_wireguard_external.id}"
+  description = "ID of the external Security Group to associate with the VPN."
 }
 
 output "vpn_asg_name" {
