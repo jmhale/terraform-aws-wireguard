c171e2d537cf9c0d1e27d691c0e9a16816d7d7be
Justin Wark <justin@predictivehire.com>
2019-08-16 09:38:25 +1000

    output 'external' Security Group
    This allows it to be referenced by other Security Groups.

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "vpn_sg_id" {
   value = "${aws_security_group.sg_wireguard_admin.id}"
   description = "ID of the internal Security Group to associate with other resources needing to be accessed on VPN."
 }
+
+output "vpn_external_sg_id" {
+  value = "${aws_security_group.sg_wireguard_external.id}"
+  description = "ID of the ('external') Security Group applied to the VPN."
+}
