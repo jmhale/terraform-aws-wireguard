7348d85ee6ebe7fe1f27ffa8a00913a3f49e9ec9
Jonny Huxtable <jonny@linkpool.io>
2020-01-20 16:22:41 +0000

    Remove EIP logic from launch config

--- main.tf
+++ main.tf
@@ -50,10 +50,10 @@ resource "aws_launch_configuration" "wireguard_launch_config" {
   image_id                    = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = var.ssh_key_id
-  iam_instance_profile        = (var.eip_id != "disabled" ? aws_iam_instance_profile.wireguard_profile[0].name : null)
+  iam_instance_profile        = aws_iam_instance_profile.wireguard_profile.name
   user_data                   = data.template_file.user_data.rendered
   security_groups             = local.security_groups_ids
-  associate_public_ip_address = (var.eip_id != "disabled" ? true : false)
+  associate_public_ip_address = true
 
   lifecycle {
     create_before_destroy = true
