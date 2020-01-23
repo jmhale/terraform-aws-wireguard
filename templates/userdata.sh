a0a17229185774bda13ae11f034c8e6dd8b92b6b
Jonny Huxtable <jonny@linkpool.io>
2020-01-23 16:40:28 +0000

    Escape the server private key

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -14,7 +14,7 @@ wg_server_private_key=$(aws ssm get-parameter \
 cat > /etc/wireguard/wg0.conf <<- EOF
 [Interface]
 Address = ${wg_server_net}
-PrivateKey = ${wg_server_private_key}
+PrivateKey = $${wg_server_private_key}
 ListenPort = ${wg_server_port}
 PostUp   = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
