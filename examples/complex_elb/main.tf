ea6439e5b9caad52d4dfd5ec9053e5032c6d23cf
James Hale <james@jameshale.net>
2020-02-21 05:35:47 -0500

    Use the ARN from the ALB target group resource to make the example more turn-key

--- examples/complex_elb/main.tf
+++ examples/complex_elb/main.tf
@@ -4,11 +4,11 @@ module "wireguard" {
   vpc_id                        = "vpc-01234567"
   additional_security_group_ids = [aws_security_group.wireguard_ssh_check.id] # for ssh health checks, see below
   subnet_ids                    = ["subnet-76543210"] # You'll want a NAT gateway on this, but we don't document that.
-  target_group_arns             = ["arn:aws:elasticloadbalancing:eu-west-1:123456789:targetgroup/wireguard-prod/123456789"]
   asg_min_size                  = 1 # a sensible minimum, which is also the default
   asg_desired_capacity          = 2 # we want two servers running most of the time
   asg_max_size                  = 5 # this cleanly permits us to allow rolling updates, growing and shrinking
   associate_public_ip_address   = false # we don't want eip, we want all our traffic out of a single NAT for whitelisting simplicity
+  target_group_arns             = [aws_lb_target_group.wireguard.arn]
   wg_server_net                 = "192.168.2.1/24" # client IPs MUST exist in this net
   wg_client_public_keys = [
     {"192.168.2.2/32" = "QFX/DXxUv56mleCJbfYyhN/KnLCrgp7Fq2fyVOk/FWU="}, # make sure these are correct
