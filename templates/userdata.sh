6145a2f8286dc96b3cb2e44ce1fd864f5cb5c261
Jonny <jonny@linkpool.io>
2020-08-10 18:16:55 +0100

    Set non interactive in userdata script to avoid hanging grub update

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -v
+export DEBIAN_FRONTEND=noninteractive
+
 add-apt-repository "ppa:wireguard/wireguard"
 apt-get update -y
-apt-get upgrade -y
+apt-get upgrade -y -o Dpkg::Options::="--force-confnew"
 apt-get install -y wireguard-dkms wireguard-tools python3-pip
 
 # aws cli
