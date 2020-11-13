ab50d6be4251ce37c909371005749bb171334fd6
Simon Garner <simon.garner@madscience.co.nz>
2020-11-14 09:02:00 +1300

    Formatting: align assignments on equals

--- examples/simple_eip/main.tf
+++ examples/simple_eip/main.tf
@@ -14,19 +14,19 @@ module "wireguard" {
   wg_server_net = "192.168.2.1/24" # client IPs MUST exist in this net
   wg_clients = [
     {
-      name = "example1"
+      name       = "example1"
       public_key = "QFX/DXxUv56mleCJbfYyhN/KnLCrgp7Fq2fyVOk/FWU="
-      client_ip = "192.168.2.2/32"
+      client_ip  = "192.168.2.2/32"
     },
     {
-      name = "example2"
+      name       = "example2"
       public_key = "+IEmKgaapYosHeehKW8MCcU65Tf5e4aXIvXGdcUlI0Q="
-      client_ip = "192.168.2.3/32"
+      client_ip  = "192.168.2.3/32"
     },
     {
-      name = "example3"
+      name       = "example3"
       public_key = "WO0tKrpUWlqbl/xWv6riJIXipiMfAEKi51qvHFUU30E="
-      client_ip = "192.168.2.4/32"
+      client_ip  = "192.168.2.4/32"
     },
   ]
 }
