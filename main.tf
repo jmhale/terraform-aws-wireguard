c881bdac33e368b1bea72758fbe57dbdc380d2ab
Jordan Sokolic <jordan@seemplicity.io>
2021-12-24 08:19:16 +0200

    upgrade to latest Ubuntu LTS (20.04)

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
