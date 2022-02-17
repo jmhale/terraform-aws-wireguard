d9b952281cb78e70a5d460e1bf303acd59742710
Jerry Aldrich <jerry@smartcontract.com>
2022-02-17 13:59:21 -0800

    Update to work with AWS provider >=4.0.0

--- main.tf
+++ main.tf
@@ -52,13 +52,13 @@ locals {
 
 # Work around user_data length limit:
 module "s3_peers_bucket" {
-  source = "git@github.com:smartcontractkit/infra-modules.git//aws/s3b?ref=a2c1fc3db306d3f3c5440abf24ba46616e7b44e7"
+  source = "git@github.com:smartcontractkit/infra-modules.git//aws/s3b?ref=d97ec52c14c45224782d4db2ba49f206fd12c6a3"
   name   = var.wireguard_bucket
   region = data.aws_region.current.name
   vpcs   = ["mgnt"]
 }
 
-resource "aws_s3_bucket_object" "peers_file" {
+resource "aws_s3_object" "peers_file" {
   bucket  = var.wireguard_bucket
   key     = "peers.txt"
   content = join("\n", data.template_file.wg_client_data_json.*.rendered)
@@ -99,26 +99,24 @@ resource "aws_autoscaling_group" "wireguard_asg" {
     create_before_destroy = true
   }
 
-  tags = [
-    {
-      key                 = "Name"
-      value               = aws_launch_configuration.wireguard_launch_config.name
-      propagate_at_launch = true
-    },
-    {
-      key                 = "Project"
-      value               = "wireguard"
-      propagate_at_launch = true
-    },
-    {
-      key                 = "env"
-      value               = var.env
-      propagate_at_launch = true
-    },
-    {
-      key                 = "tf-managed"
-      value               = "True"
-      propagate_at_launch = true
-    },
-  ]
+  tag {
+    key                 = "Name"
+    value               = aws_launch_configuration.wireguard_launch_config.name
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "Project"
+    value               = "wireguard"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "env"
+    value               = var.env
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "tf-managed"
+    value               = "True"
+    propagate_at_launch = true
+  }
 }
