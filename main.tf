578223c303aa8f6377664edd903467645a359b86
Pedro Neto <pedro@superlayer.co>
2023-10-02 11:30:52 +0100

    🧱 change ubuntu from 16 to 22

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
