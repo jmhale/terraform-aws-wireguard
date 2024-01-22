1319c02fef389c1345deeaf91ae891e4f87c69e7
william <william@williampietri.com>
2024-01-21 19:19:06 -0800

    Update tags to TF 1.6 format

--- main.tf
+++ main.tf
@@ -77,26 +77,24 @@ resource "aws_autoscaling_group" "wireguard_asg" {
     create_before_destroy = true
   }
 
-  tags = [
-    {
+  tag {
       key                 = "Name"
       value               = aws_launch_configuration.wireguard_launch_config.name
       propagate_at_launch = true
-    },
-    {
+  }
+  tag {
       key                 = "Project"
       value               = "wireguard"
       propagate_at_launch = true
-    },
-    {
+  }
+  tag {
       key                 = "env"
       value               = var.env
       propagate_at_launch = true
-    },
-    {
+  }
+  tag {
       key                 = "tf-managed"
       value               = "True"
       propagate_at_launch = true
-    },
-  ]
+  }
 }
